43a2120d9692dcfe5c0918a7eab025d87cd369bc
Mizar Contasti <contasti.mizar@gmail.com>
2023-05-04 22:16:53 -0600

    style uploaded

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -12,8 +12,8 @@ module.exports = function () {
             <df-messenger
               intent="WELCOME"
               chat-icon="https://codexceleste.com/chatbotsOld/core/images/zaroc1.png"
-              chat-title="Zaroc"
-              agent-id="7f37a296-936b-4e34-93ce-10b50bdb8b97"
+              chat-title="Chasire"
+              agent-id="b09c4b7c-fe00-46fd-8b03-dbed2ab76bdc"
               language-code="es"
             ></df-messenger>
               `,
